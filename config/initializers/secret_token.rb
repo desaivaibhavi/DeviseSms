6beaecb6c7fdf06e09cc3bb9f262baca121c4c2d
Yash Shah <mail@yashshah.com>
2013-09-24 00:16:57 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseSms::Application.config.secret_token = '44bf674a2de3d242369799a1da0b4d0edea65462aa02ee31109c36e93c5cc5d575cc49e083948ae182a95de32d3b47474bf7b776df9f080d989defe6a2946e9d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DeviseSms::Application.config.session_store :cookie_store, key: '_DeviseSms_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DeviseSms::Application.config.session_store :active_record_store
